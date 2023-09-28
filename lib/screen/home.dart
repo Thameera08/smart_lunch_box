b252e3b9bc366523a4ddd4a08a501d183c16d968
thameera08 <thamilahiru@gmail.com>
2023-09-28 13:22:46 +0530

    re fix

--- lib/screen/home.dart
+++ lib/screen/home.dart
@@ -1,41 +1,15 @@
 import 'package:flutter/material.dart';
 
-class Home extends StatelessWidget {
+class Home extends StatefulWidget {
   const Home({super.key});
 
   @override
+  State<Home> createState() => _HomeState();
+}
+
+class _HomeState extends State<Home> {
+  @override
   Widget build(BuildContext context) {
-    return const Padding(
-      padding: EdgeInsets.all(24.0),
-      child: Column(
-        children: [
-          Text(
-            ' Smart Lunch Box',
-            style: TextStyle(
-              fontSize: 40,
-              color: Color.fromARGB(255, 53, 52, 52),
-              fontWeight: FontWeight.bold,
-            ),
-          ),
-          SizedBox(
-            height: 100,
-          ),
-          CircleAvatar(
-            radius: 70,
-            child: Icon(
-              Icons.lunch_dining,
-              size: 120,
-            ),
-          ),
-          SizedBox(
-            height: 100,
-          ),
-          Text(
-            'Home Screen',
-            style: TextStyle(fontSize: 30, color: Colors.white),
-          ),
-        ],
-      ),
-    );
+    return Container();
   }
 }
